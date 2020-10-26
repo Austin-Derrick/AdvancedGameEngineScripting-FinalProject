a1214ac7f5ca66c677f55883a7a483238d4d5459
Austin-Derrick <aderrickson@colum.edu>
2020-10-26 17:52:17 -0500

    Custom script to shoot the gun

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    public GameObject bulletImpact;
+
+    public int currentAmmo = 10;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (currentAmmo > 0)
+            {
+                Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Instantiate(bulletImpact, hit.point, transform.rotation);
+                    //Debug.Log("I'm looking at " + hit.transform.name);
+                }
+                else
+                {
+                    Debug.Log("I'm looking at nothing");
+                }
+                currentAmmo--;
+            }
+        }
+    }
+}
