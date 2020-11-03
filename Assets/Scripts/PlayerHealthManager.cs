057ae2092fe042d3636b12642e6da0cfdd613f6c
Austin-Derrick <aderrickson@colum.edu>
2020-11-02 19:38:26 -0600

    Made player health manager script so the player could be hurt by the enemy.

---
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public int currentHealth;
+
+    public int maxHealth = 100;
+
+    private bool hasDied = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasDied)
+        {
+            gameObject.GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
+
+    public void takeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            // GameOverScript
+            hasDied = true;
+        }
+    }
+
+    public void addHealth(int healthAmount)
+    {
+        currentHealth += healthAmount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
