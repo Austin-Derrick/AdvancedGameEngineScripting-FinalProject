bc1706bda619a6a70cc0d3e5401cdfa6cc2935fd
Austin-Derrick <aderrickson@colum.edu>
2020-11-02 19:37:18 -0600

    Adjusted shoot script to have the enemy take damage.

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    public static Shoot instance;
+
     public Camera mainCamera;
 
     public GameObject bulletImpact;
 
     public int currentAmmo = 10;
+
+    public Animator gunAnim;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -23,12 +33,18 @@ public class Shoot : MonoBehaviour
                 {
                     Instantiate(bulletImpact, hit.point, transform.rotation);
                     //Debug.Log("I'm looking at " + hit.transform.name);
+
+                    if (hit.transform.tag == "Enemy")
+                    {
+                        hit.transform.GetComponent<EnemyController>().takeDamage();
+                    }
                 }
                 else
                 {
                     Debug.Log("I'm looking at nothing");
                 }
                 currentAmmo--;
+                gunAnim.SetTrigger("Shoot");
             }
         }
     }
